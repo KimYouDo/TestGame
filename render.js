1f2ea98da2ef20fa2536ec43a8989e46c50d818f
KimYouDo <32061808+KimYouDo@users.noreply.github.com>
2018-07-16 18:05:01 +0900

    Add files via upload

---
+++ render.js
@@ -0,0 +1,13 @@
+var img1 = new Image();
+var img2 = new Image();
+var img3 = new Image();
+var Rturn = new Image();
+var Lturn = new Image();
+var back = new Image();
+
+back.src="https://raw.githubusercontent.com/KimYouDo/TestGame/master/back0.png";
+Lturn.src="https://raw.githubusercontent.com/KimYouDo/TestGame/master/Lturn.png";
+Rturn.src="https://raw.githubusercontent.com/KimYouDo/TestGame/master/Rturn.png";
+img1.src="https://raw.githubusercontent.com/KimYouDo/TestGame/master/bas0.png";
+img2.src="https://raw.githubusercontent.com/KimYouDo/TestGame/master/bas1.png";
+img3.src="https://raw.githubusercontent.com/KimYouDo/TestGame/master/bas2.png";
